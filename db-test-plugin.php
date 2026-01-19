6ec61dd86ea86bf51f7f3236ac70ca43e700439f
unknown <hoangson.13.8.97@gmail.com>
2026-01-19 12:17:01 +0700

    đổi nội dung file chính

--- db-test-plugin.php
+++ db-test-plugin.php
@@ -1,6 +1,22 @@
 <?php
-// plugins/managed/test-plugin/test-plugin.php
+/**
+ * Plugin Name: Hello World Simple
+ * Plugin URI: https://github.com/USERNAME/hello-world-simple
+ * Description: Plugin WordPress đơn giản hiển thị Hello World.
+ * Version: 1.0.0
+ * Author: Your Name
+ * Author URI: https://github.com/USERNAME
+ * License: GPL v2 or later
+ */
 
-add_action('init', function () {
-    error_log('Managed Test Plugin loaded');
-});
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Hiển thị Hello World ở frontend
+ */
+function hws_show_hello_world() {
+    return '<h2 style="color:green">Hello World 👋</h2>';
+}
+add_shortcode('hello_world', 'hws_show_hello_world');
